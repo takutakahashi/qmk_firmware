542875c50dafe541a0bf7ff1f487b9ea5dc7b37c
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2019-02-23 23:41:49 +0900

    mod mouse key

--- keyboards/ergo42/keymaps/default/keymap.c
+++ keyboards/ergo42/keymaps/default/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,   KC_QUOT,         KC_GRV, KC_Y,   KC_U,    KC_I,     KC_O,    KC_P,    KC_BSPC,\
     KC_Q,     KC_A,    KC_S,   KC_D,    KC_F,    KC_G,   KC_MINS,         KC_EQL, KC_H,   KC_J,    KC_K,     KC_L,    KC_SCLN, KC_ENT, \
     KC_LSFT,  XXXXXXX, KC_Z,   KC_X,    KC_C,    KC_V,   KC_B   ,         KC_RBRC,KC_N,   KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_BSLS,\
-    MO(META), KC_LCTL, MO(SYMB),KC_LALT,KC_LGUI, KC_SPC, KC_LSFT,         KC_SPC, KC_RCTL,MO(META),KC_LANG1, MO(META),MO(META),MO(META) \
+    MO(META), KC_LCTL, KC_LALT,MO(SYMB),KC_LGUI, KC_SPC, KC_LSFT,         KC_SPC, KC_RCTL,MO(META),KC_LANG1, MO(META),MO(META),MO(META) \
   ),
 
   /* META
